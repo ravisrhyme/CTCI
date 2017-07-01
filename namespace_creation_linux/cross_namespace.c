ef7651f0939826e8b59e00ad854075ba97a6649b
Ravi Kiran Chadalawada <ravisrhyme@gmail.com>
2017-06-30 18:32:46 -0700

    Demo of creating namespaces

---
+++ namespace_creation_linux/cross_namespace.c
@@ -0,0 +1,40 @@
+/****************************************************************
+Program to create new cross namespace.
+Demo of using unshare() system call provided by linux.
+unshare() helps in creating an isolated namespace from parent.
+
+Need to compiled binary as root to see namespace creation
+
+Credits : Mahmud Ridwan
+*****************************************************************/
+
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+static char child_stack[1048576];
+
+static int child_fn() {
+  // calling unshare() from inside the init process lets you create a new namespace after a new process has been spawned
+  unshare(CLONE_NEWNET);
+
+  printf("New `net` Namespace:\n");
+  system("ip link");
+  printf("\n\n");
+  return 0;
+}
+
+int main() {
+  printf("Original `net` Namespace:\n");
+  system("ip link");
+  printf("\n\n");
+
+  pid_t child_pid = clone(child_fn, child_stack+1048576, CLONE_NEWPID | SIGCHLD, NULL);
+
+  waitpid(child_pid, NULL, 0);
+  return 0;
+}
---
+++ namespace_creation_linux/network_namespace.c
@@ -0,0 +1,37 @@
+/****************************************************************
+Program to create new process and network namespace.
+
+Need to compiled binary as root to see namespace creation
+
+Credits : Mahmud Ridwan
+*****************************************************************/
+
+
+
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+static char child_stack[1048576];
+
+static int child_fn() {
+  printf("New `net` Namespace:\n");
+  system("ip link");
+  printf("\n\n");
+  return 0;
+}
+
+int main() {
+  printf("Original `net` Namespace:\n");
+  system("ip link");
+  printf("\n\n");
+
+  pid_t child_pid = clone(child_fn, child_stack+1048576, CLONE_NEWPID | CLONE_NEWNET | SIGCHLD, NULL);
+
+  waitpid(child_pid, NULL, 0);
+  return 0;
+}
